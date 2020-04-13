af28c01a815a7a7bff3bb0c5f7fd75475b4be858
Grigoriy Skobelev <gskobelevff@gmail.com>
2020-04-13 10:34:53 +0300

    init commit

---
+++ src/main/kotlin/com/baltic/road/path/app/RoadPathApplication.kt
@@ -0,0 +1,11 @@
+package com.baltic.road.path.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoadPathApplication
+
+fun main(args: Array<String>) {
+    runApplication<RoadPathApplication>(*args)
+}
---
+++ src/test/kotlin/com/baltic/road/path/app/RoadPathApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.baltic.road.path.app
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoadPathApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
